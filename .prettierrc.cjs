e1241bbbe36926accde8cffd6a96cc83895ef843
swzx00 <68842140+swzx00@users.noreply.github.com>
2024-11-20 15:07:49 +0800

    chore/base: Prettier init
    ------
    
    $ npm install -D prettier eslint-config-prettier eslint-plugin-prettier
    > 安裝 Prettier 套件
      直接安裝 prettier、eslint-config-prettier 與 eslint-plugin-prettier 三個套件。
    
    > 配置 Prettier 設定檔e# Please eer 設定檔案
      我們在專案根目錄下建立 .prettierrc.cjs 檔案，內容如下，這些配置都可以自己設定來配合
      ESLint，更多選項或說明可以參考 Prettier 官方說明文件。
    
      > module.exports = {
           printWidth: 100,          // 每行文字數量達 100 字元就換到新的一行
           semi: false,              // 每個語句的結尾不需要分號
           singleQuote: true,        // 字串使用單引號，而不是雙引號
           trailingComma: 'none'     // 如 Object、Array 內的元素不需要尾隨逗號
        }
    
    > 配置 ESLint 設定檔案
      我們在安裝時多裝了兩個 ESLint 相關套件，分別為 eslint-config-prettier
      及 eslint-plugin-prettier。
      > .eslintrc.cjs 設定檔應該會如下
      > module.exports = {
           env: {
               browser: true,
               es2023: true
           },
           extends: ['@nuxtjs/eslint-config-typescript','prettier'],
           parserOptions: {
              ecmaVersion: 2023,
              sourceType: 'module'
           },
           plugins: ['prettier'],
           rules: {
              'no-undef': 'off',
              'prettier/prettier': 'error'
           }
        }
    
    > 安裝 VS Code 的 Prettier 插件
    > [https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode]
    > Prettier - Code formatter:
      提供我們做程式碼的格式化，最重要的是來協助我們自動載入 .prettierrc.js
      配置。

---
+++ .prettierrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  printWidth: 120, // 每行文字數量達 120 字元就換到新的一行
+  semi: false, // 每個語句的結尾需要分號
+  singleQuote: true, // 字串使用單引號，而不是雙引號
+  trailingComma: "all", // 如 Object、Array 內的元素需要尾隨逗號
+}
