6a6ac17f7031079720f98b37b9a39dde5d8a9923
swzx00 <68842140+swzx00@users.noreply.github.com>
2024-11-20 16:27:34 +0800

    chore/base: EndOfLine all LF
    -------
    
    > vscode setting 搜尋 eol 選擇 [預設行尾字元]>[[\n]
    
    > 通常以 windows for git 無腦安裝時，
      core.autocrlf 預設選項是 true。
    
    > 檢查 core.autocrlf 的設定，通常都是 true
    $ git config --get core.autocrlf
    
    > 針對專案設定 git 的切換模式為 false 不切換
    $ git config core.autocrlf false
    
    > 讓 git 重抓檔案與以新設定處理換行字元
    $ git rm --cached -r .
    
    > 但不儲存
    $ git reset --hard
    
    > setting.json
      {
        "files.eol": "auto",
      }

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -3,4 +3,5 @@ module.exports = {
   semi: false, // 每個語句的結尾需要分號
   singleQuote: true, // 字串使用單引號，而不是雙引號
   trailingComma: "all", // 如 Object、Array 內的元素需要尾隨逗號
+  endOfLine: "lf",
 }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   typescript: {
-    typeCheck: true
-  }
-})
+    typeCheck: true,
+  },
+});
