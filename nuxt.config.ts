c029b32b105e14be98fb9f651877d6ce5a8b128e
swzx00 <68842140+swzx00@users.noreply.github.com>
2024-11-22 14:58:22 +0800

    chore/base: Google Font init
    ------
    
    $ npx nuxi@latest module add google-fonts
    
    > // nuxt.config.ts
      export default defineNuxtConfig({
        modules: ['@nuxtjs/google-fonts'],
        googleFonts: {
          families: {
            // 指定您想要的字體
            'Noto Sans': [100, 200, 300, 400, 500, 600, 700, 800, 900], // 指定需要的字體粗細
          },
        },
      })

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,13 @@ export default defineNuxtConfig({
   typescript: {
     typeCheck: true,
   },
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt', '@nuxtjs/google-fonts'],
+  googleFonts: {
+    families: {
+      // 指定您想要的字體
+      'Noto Sans': [100, 200, 300, 400, 500, 600, 700, 800, 900], // 指定需要的字體粗細
+    },
+  },
   // dir: { // 自訂目錄名稱
   //   layouts: 'layouts',
   //   middleware: 'middleware',
