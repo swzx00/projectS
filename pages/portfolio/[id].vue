c3da9a6b7f09f59e8f0dfefdc40d1fb9735219d4
swzx00 <68842140+swzx00@users.noreply.github.com>
2024-12-26 13:40:32 +0800

    feat/portfolio: fixed dataCard type of

--- pages/portfolio/[id].vue
+++ pages/portfolio/[id].vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
 import { useSingleDataFetch } from '~/composables/useSingleDataFetch'
 import type { ResponseData } from '~/composables/interface'
 
 const route = useRoute()
 const data = ref<ResponseData | null>(null)
-const pending = ref(true)
+const pending = ref(false)
 const error = ref<Error | null>(null)
 const dataLoaded = ref(false)
-const items = ref<ResponseData['dataCard'][0] | null>(null) // 指定為 dataCard 的單一項目或 null
+const item = ref<any>(null) // 指定為 any 或 null
 
 onMounted(async () => {
   const safeId = route.params.id as string
@@ -16,8 +18,7 @@ onMounted(async () => {
     data.value = response.data
     pending.value = false
 
-    // 確保 dataCard 的資料結構符合型別定義
-    items.value = data.value?.dataCard[0] ?? null
+    item.value = data.value?.dataCard[0] ?? null
   } catch (err) {
     error.value = err as Error
     pending.value = false
@@ -52,37 +53,37 @@ const updateImageSize = (index: number, event: Event) => {
         載入資料時出現錯誤：<span class="text-red-600">{{ error }}</span>
       </p>
       <div
-        v-if="dataLoaded && data && items"
+        v-if="dataLoaded && data && item"
         class="flex w-full flex-col items-start justify-start gap-0 !px-0 pt-4 md:container md:max-w-full lg:max-w-[992px] xl:max-w-[1200px] 2xl:max-w-[1400px]"
       >
-        <BreadCrumb :card-id="items.id" :title="items.title"></BreadCrumb>
+        <BreadCrumb :card-id="item.id" :title="item.title"></BreadCrumb>
         <div class="flex w-full max-w-full flex-row items-end justify-between gap-0 px-4 lg:px-0">
           <h1 class="inline-block w-fit max-w-[calc(100%-6rem)] break-words text-2xl font-bold text-black">
-            {{ items.title }}
+            {{ item.title }}
           </h1>
-          <span class="inline-block w-24 max-w-full text-end text-sm font-light text-gray-900">{{ items.date }}</span>
+          <span class="inline-block w-24 max-w-full text-end text-sm font-light text-gray-900">{{ item.date }}</span>
         </div>
         <div class="mt-4 flex w-full max-w-full flex-wrap items-start justify-start gap-x-3 gap-y-2 px-4 lg:px-0">
           <span
-            v-for="(item, index) in items.tag"
+            v-for="(tag, index) in item.tag"
             :key="index"
             class="cursor-default text-start text-base font-light capitalize leading-none tracking-normal text-blue-500"
-            >#{{ item }}</span
+            >#{{ tag }}</span
           >
         </div>
         <main
           class="mb-0 mt-8 w-full max-w-full border-x-0 border-y border-solid border-white bg-white shadow-lg lg:mb-12 lg:border"
         >
           <picture
-            v-for="(item, index) in items.image"
+            v-for="(image, index) in item.image"
             :key="index"
             class="block w-full max-w-full border-x-0 border-y border-solid border-white lg:border-x lg:border-y-2"
           >
             <img
               class="object-contain"
-              :src="item"
-              :title="`${items.title}-${index + 1}`"
-              :alt="`${items.title}-${index + 1}`"
+              :src="image"
+              :title="`${item.title}-${index + 1}`"
+              :alt="`${item.title}-${index + 1}`"
               :width="imageSizes[index]?.width || ''"
               :height="imageSizes[index]?.height || ''"
               loading="lazy"
@@ -94,7 +95,7 @@ const updateImageSize = (index: number, event: Event) => {
             class="article flx-col mb-12 mt-8 px-4 text-start text-base font-normal leading-normal lg:my-12 lg:px-8"
           >
             <p
-              v-for="(line, index) in items.content.split('\n')"
+              v-for="(line, index) in item.content.split('\n')"
               :key="index"
               class="my-2 block w-fit max-w-full first:mt-0 last:mb-0"
             >
