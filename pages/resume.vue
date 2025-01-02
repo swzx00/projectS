f5a64fdf564ba99c44f935fe5bcd6945541e3297
swzx00 <68842140+swzx00@users.noreply.github.com>
2025-01-03 00:45:19 +0800

    feat/resume: add resume

--- pages/resume.vue
+++ pages/resume.vue
@@ -18,7 +18,7 @@ onMounted(async () => {
     if (response.data) {
       data.value = response.data.dataResume[0]
       introductionLines.value = data.value.introduction.split('\n')
-      autobiographyLines.value = data.value.introduction.split('\n')
+      autobiographyLines.value = data.value.autobiography.split('\n')
 
       // 處理 experience 的 end 字段
       data.value.experience = data.value.experience.map((exp) => {
@@ -42,7 +42,7 @@ onMounted(async () => {
 const { isImageLoaded, imageRef, handleImageLoad } = useImageLoading()
 </script>
 <template>
-  <div class="relative size-full max-h-full max-w-full bg-gray-200" style="border: 1px solid red">
+  <div class="relative box-border size-full max-h-full max-w-full border border-solid border-gray-200 bg-gray-200">
     <p
       v-if="pending"
       class="mx-auto my-10 flex w-full max-w-full items-center justify-start gap-2 text-lg text-gray-600 md:max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg 2xl:max-w-screen-xl"
@@ -168,65 +168,254 @@ const { isImageLoaded, imageRef, handleImageLoad } = useImageLoading()
             </p>
             <ul class="flex flex-row flex-wrap items-start justify-start gap-0">
               <li
-                v-for="(content, indexContent) in skill['skills-content']"
-                :key="indexContent"
+                v-for="(content, idx) in skill['skills-content']"
+                :key="idx"
                 class="relative flex flex-col items-center justify-center text-sm font-normal text-gray-950"
                 :class="typeof content !== 'string' ? 'my-1 px-1' : ''"
               >
                 <template v-if="typeof content === 'string'">{{ content }}</template>
-                <!-- <template v-else>
-                  {{ content['skills-content-title'] }} - {{ content['skills-content-proficiency'] }}%
-                </template> -->
                 <template v-else>
                   <div
-                    class="relative aspect-square size-[6.5rem] rounded-full bg-blue-600 before:z-0 before:ml-[50%] before:block before:h-full before:origin-left before:rotate-[--rotate-turn] before:rounded-[0_100%_100%_0/50%] before:bg-blue-600 before:content-[''] after:absolute after:left-1/2 after:top-1/2 after:z-10 after:block after:size-[5.5rem] after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full after:bg-white after:content-['']"
+                    class="aos before:bg-blue-600"
+                    data-aos="aos-rotate"
+                    data-aos-delay="500"
                     :style="{
-                      '--rotate-turn': `${Number(content['skills-content-proficiency']) / 100}turn`,
-                      'background-image': 'linear-gradient(to right, transparent 50%, rgba(229, 231,235, 1) 0%)',
+                      '--size': '6.5rem',
+                      '--rotate-deg': `${(Number(content['skills-content-proficiency']) / 100) * 360}deg`,
                     }"
+                  ></div>
+                  <div
+                    class="aos-font absolute z-[3] aspect-square size-[5.5rem] rounded-full bg-white"
+                    data-aos="aos-font"
+                    data-aos-delay="1000"
                   >
                     <p
-                      class="tracking-none absolute left-1/2 top-[30%] z-20 mt-2 flex w-fit -translate-x-1/2 items-center justify-center bg-white text-center text-lg font-normal leading-none tracking-tighter"
+                      class="absolute left-1/2 top-[22.5%] z-20 mt-2 flex w-fit -translate-x-1/2 items-center justify-center text-center text-lg font-normal leading-none tracking-tighter"
                     >
                       {{ content['skills-content-title'] }}
                     </p>
                     <p
-                      class="absolute left-1/2 top-[57.5%] z-20 -translate-x-1/2 text-center text-base font-semibold after:text-xs after:font-light after:content-['%']"
+                      class="absolute left-1/2 top-[50%] z-20 -translate-x-1/2 text-center text-base font-semibold after:text-xs after:font-light after:content-['%']"
                     >
                       {{ content['skills-content-proficiency'] }}
                     </p>
                   </div>
                 </template>
-                <template v-if="indexContent < skill['skills-content'].length - 1 && typeof content === 'string'"
-                  >、</template
-                >
+                <template v-if="idx < skill['skills-content'].length - 1 && typeof content === 'string'">、</template>
               </li>
             </ul>
           </li>
         </ul>
       </section>
-      <!-- <section
-        class="flex w-full max-w-full flex-row flex-nowrap items-start justify-between gap-4 border-b border-solid border-gray-100 py-6"
+      <section
+        class="flex w-full max-w-full flex-col flex-wrap items-start justify-between gap-4 border-b border-solid border-gray-100 py-6"
       >
-        <div class="flex flex-row gap-2 text-base text-gray-900">
-          <p class="font-semibold">出生日期</p>
-          <p class="font-normal">{{ data.birthday }}</p>
+        <h2 class="inline-block w-full max-w-full text-xl font-black leading-relaxed text-gray-900">
+          自傳
+          <span class="ml-2 inline-block text-lg font-extrabold text-gray-800">Autobiography</span>
+        </h2>
+        <div class="block">
+          <p
+            v-for="(line, index) in autobiographyLines"
+            :key="index"
+            class="mb-3 text-start text-base font-normal leading-normal text-gray-800 last:mb-0"
+          >
+            {{ line }}
+          </p>
         </div>
-        <div class="flex flex-row gap-2 text-base text-gray-900">
-          <p class="font-semibold">居住地</p>
-          <p class="font-normal">{{ data.residence }}</p>
+      </section>
+      <section
+        class="flex w-full max-w-full flex-col flex-wrap items-start justify-between gap-4 border-b border-solid border-gray-100 py-6"
+      >
+        <h2 class="inline-block w-full max-w-full text-xl font-black leading-relaxed text-gray-900">
+          作品集
+          <span class="ml-2 inline-block text-lg font-extrabold text-gray-800">Portfolio</span>
+        </h2>
+        <p class="mb-3 text-start text-base font-normal leading-normal text-gray-800">
+          <span
+            class="mr-2 inline-flex items-center justify-start font-medium after:inline after:content-['：'] xl:mr-0"
+            >線上作品集</span
+          >
+          <a
+            class="text-blue-600 underline underline-offset-2 transition-all duration-300 hover:text-blue-800 hover:underline-offset-4"
+            :href="data.portfolios[0].url"
+            title="線上作品集"
+            alt="線上作品集"
+            target="_blank"
+            >{{ data.portfolios[0].url }}</a
+          >
+        </p>
+      </section>
+      <section class="flex w-full max-w-full flex-col flex-wrap items-start justify-between gap-4 py-6">
+        <h2 class="inline-block w-full max-w-full text-xl font-black leading-relaxed text-gray-900">
+          聯絡方式
+          <span class="ml-2 inline-block text-lg font-extrabold text-gray-800">Information</span>
+        </h2>
+        <div class="flex w-full max-w-full flex-col flex-wrap items-start justify-start gap-2 sm:flex-row sm:gap-0">
+          <p
+            class="mb-0 inline-flex w-full flex-nowrap text-start text-base font-normal leading-normal text-gray-800 sm:w-1/2 xl:w-1/4"
+          >
+            <span
+              class="mr-2 inline-flex w-20 justify-between gap-0 text-justify font-medium after:inline after:content-['：'] xl:mr-0 xl:w-fit"
+              >聯繫時間</span
+            >
+            <span class="text-gray-800 no-underline hover:text-gray-800">{{ data['contact-time'] }}</span>
+          </p>
+          <p
+            class="mb-0 inline-flex w-full flex-nowrap text-start text-base font-normal leading-normal text-gray-800 sm:w-1/2 xl:w-1/4"
+          >
+            <span
+              class="mr-2 inline-flex w-20 justify-between gap-0 text-justify font-medium after:inline after:content-['：'] xl:mr-0 xl:w-fit"
+              >聯繫電話</span
+            >
+            <a
+              class="text-gray-800 no-underline hover:text-gray-800"
+              :href="`tel:${data.phone}`"
+              title="聯繫電話"
+              alt="聯繫電話"
+              target="_self"
+              >{{ data.phone }}</a
+            >
+          </p>
+          <p
+            class="mb-0 inline-flex w-full flex-nowrap text-start text-base font-normal leading-normal text-gray-800 sm:w-1/2 xl:w-1/4"
+          >
+            <span
+              class="mr-2 inline-flex w-20 justify-between gap-0 text-justify font-medium after:inline after:content-['：'] xl:mr-0 xl:w-fit"
+              >E-mai</span
+            >
+            <a
+              class="text-blue-600 underline underline-offset-2 transition-all duration-300 hover:text-blue-800 hover:underline-offset-4"
+              :href="`mailto:${data.email}`"
+              title="E-mail"
+              alt="E-mail"
+              target="_self"
+              >{{ data.email }}</a
+            >
+          </p>
+          <p
+            class="mb-0 inline-flex w-full flex-nowrap text-start text-base font-normal leading-normal text-gray-800 sm:w-1/2 xl:w-1/4"
+          >
+            <span
+              class="mr-2 inline-flex w-20 justify-between gap-0 text-justify font-medium after:inline after:content-['：'] xl:mr-0 xl:w-fit"
+              >居住地</span
+            >
+            <span class="text-gray-800 hover:text-gray-800">{{ data.residence }}</span>
+          </p>
         </div>
-        <div class="flex flex-row gap-2 text-base text-gray-900">
-          <p class="font-semibold">婚姻狀況</p>
-          <p class="font-normal">{{ data.marital }}</p>
-        </div>
-        <div class="flex flex-row gap-2 text-base text-gray-900">
-          <p class="font-semibold">兵役狀況</p>
-          <p class="font-normal">{{ data.military['military-type'] }} ({{ data.military['military-discharge'] }})</p>
-        </div>
-      </section> -->
+      </section>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.aos {
+  position: relative;
+  left: 0;
+  top: 0;
+  height: var(--size);
+  width: var(--size);
+  overflow: hidden;
+  border-radius: 999px;
+  background-color: transparent;
+}
+.aos:before,
+.aos:after {
+  content: '';
+  box-sizing: border-box;
+  position: absolute;
+  top: 0;
+  right: calc(var(--size) / 2);
+  width: calc(var(--size) / 2);
+  height: var(--size);
+  border-radius: calc(var(--size) / 2) 0 0 calc(var(--size) / 2);
+  transform-origin: calc(var(--size) / 2) calc(var(--size) / 2);
+  opacity: 0;
+}
+.aos:before {
+  z-index: 1;
+  transform: rotate(0deg);
+}
+.aos:after {
+  z-index: 2;
+  transform: rotate(0deg);
+}
+.aos[data-aos='aos-rotate'].aos-animate {
+  animation: aos-bg 500ms 1 linear forwards;
+}
+.aos[data-aos='aos-rotate'].aos-animate::before {
+  animation: aos-before 500ms 1 linear forwards;
+}
+.aos[data-aos='aos-rotate'].aos-animate::after {
+  animation: aos-after 500ms 1 step-start forwards;
+}
+.aos[data-aos='aos-rotate'][data-aos-delay='500'].aos-animate::before {
+  animation-delay: 500ms;
+}
+.aos[data-aos='aos-rotate'][data-aos-delay='500'].aos-animate::after {
+  animation-delay: 500ms;
+}
+.aos-font[data-aos='aos-font'] * {
+  opacity: 0;
+}
+.aos-font[data-aos='aos-font'].aos-animate * {
+  animation: aos-font 500ms 1 linear forwards;
+}
+.aos-font[data-aos='aos-font'][data-aos-delay='1000'].aos-animate * {
+  animation-delay: 1000ms;
+}
+@keyframes aos-bg {
+  0% {
+    opacity: 0;
+    background-color: #ffffff;
+  }
+  100% {
+    opacity: 1;
+    background-color: #e5e7eb;
+  }
+}
+@keyframes aos-before {
+  0% {
+    opacity: 1;
+    transform: rotate(0deg);
+  }
+  50% {
+    opacity: 1;
+    transform: rotate(180deg);
+  }
+  100% {
+    opacity: 1;
+    transform: rotate(var(--rotate-deg));
+  }
+}
+@keyframes aos-after {
+  0% {
+    opacity: 1;
+    background-color: #e5e7eb;
+    transform: rotate(0deg);
+  }
+  49.99% {
+    opacity: 1;
+    background-color: #e5e7eb;
+    transform: rotate(0deg);
+  }
+  50% {
+    opacity: 1;
+    background-color: #2563eb;
+    transform: rotate(180deg);
+  }
+  100% {
+    opacity: 1;
+    background-color: #2563eb;
+    transform: rotate(180deg);
+  }
+}
+@keyframes aos-font {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
