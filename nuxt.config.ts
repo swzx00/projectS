4c8a5043570afd0d0dcdf62a1d3a096e8404a5e2
swzx00 <68842140+swzx00@users.noreply.github.com>
2025-01-02 17:24:43 +0800

    chore/base: add nuxt-aos
    ------
    
    -npx nuxi@latest module add aos

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,7 @@ export default defineNuxtConfig({
     '@pinia-plugin-persistedstate/nuxt',
     '@nuxtjs/google-fonts',
     '@nuxt/icon',
+    'nuxt-aos',
   ],
   googleFonts: {
     families: {
