3e048a311b2948a14d9672e35c4ba983c0876e62
swzx00 <68842140+swzx00@users.noreply.github.com>
2025-03-11 11:32:44 +0800

    fix/design: container border show to test

--- components/Card/CardDesign.vue
+++ components/Card/CardDesign.vue
@@ -84,6 +84,7 @@ const onLeave = () => {
 <template>
   <div
     class="group flex w-full max-w-full flex-col flex-nowrap items-start justify-start overflow-hidden rounded bg-transparent transition-all duration-300 ease-linear"
+    style="border: 3px solid cyan"
     @mouseenter="onHover"
     @mouseleave="onLeave"
   >
@@ -91,9 +92,11 @@ const onLeave = () => {
       class="flex aspect-square size-full max-h-max min-h-fit min-w-fit max-w-full items-center justify-center rounded bg-zinc-700/80 p-4"
       :to="`/portfolio/${cardId}?from=design`"
       target="_blank"
+      style="border: 3px solid pink"
     >
       <div
         class="flex aspect-square size-full max-h-fit min-h-full min-w-full max-w-full items-center justify-center overflow-hidden rounded-sm bg-white/80 p-0 group-hover:bg-white/100"
+        style="border: 3px solid red"
       >
         <Icon
           v-if="!isImageLoaded"
@@ -119,6 +122,7 @@ const onLeave = () => {
     </NuxtLink>
     <div
       class="card-body flex size-full max-h-full max-w-full flex-col flex-nowrap items-start justify-start gap-1.5 overflow-hidden py-2"
+      style="border: 3px solid red"
     >
       <h3
         class="m-0 inline-block w-fit max-w-full cursor-default truncate p-0 text-start text-base font-bold text-zinc-50/80 group-hover:text-zinc-50/100"
