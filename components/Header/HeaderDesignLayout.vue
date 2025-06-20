d9645d38e39d4bfc6ad0bb392f5ff913c8edb260
swzx00 <68842140+swzx00@users.noreply.github.com>
2025-06-20 15:33:14 +0800

    fix/design: fix wrong words

--- components/Header/HeaderDesignLayout.vue
+++ components/Header/HeaderDesignLayout.vue
@@ -2,7 +2,7 @@
 
 <template>
   <header
-    class="rlative mx-0 h-8 min-h-8 w-full max-w-full border-b border-solid border-zinc-800 bg-zinc-600 px-4 py-1"
+    class="relative mx-0 h-8 min-h-8 w-full max-w-full border-b border-solid border-zinc-800 bg-zinc-600 px-4 py-1"
   >
     <h1 class="hidden">Design</h1>
     <div class="flex h-full flex-row flex-nowrap items-stretch justify-start *:flex *:h-full *:items-center">
